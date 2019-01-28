e26834a72ec5d424c4ed4bb24e1cebdbd20bcfc3
Miriam Wagner <miriam.wagner@wagner-dn.de>
2019-01-28 14:29:25 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,8 @@ var playlist = {firstSong: "Smoorverliefd"}
 function updatePlaylist(playlist, artistName, songTitle) {
   return playlist[artistName] = songTitle
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+  return playlist
+}
